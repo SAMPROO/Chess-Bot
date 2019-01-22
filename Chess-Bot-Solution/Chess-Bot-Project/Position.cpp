b934b6521a5b59e33b525d6ccc09d0b8006c6709
SAMPROO <sampo.tuisku@gmail.com>
2019-01-22 18:40:40 +0200

    Comments

--- Chess-Bot-Solution/Chess-Bot-Project/Position.cpp
+++ Chess-Bot-Solution/Chess-Bot-Project/Position.cpp
@@ -1,19 +1,22 @@
 #include "Position.h"
 
 Position::Position() {
-	ChessPiece * VK = new ChessPiece("\u2654", 0, VK);
+	/*ChessPiece * VK = new ChessPiece("\u2654", 0, VK);
+	board[0][0] = new ChessPiece("\u2656", 0, WR);
+	board[0][1] = new ChessPiece();
+	board[0][1]->setUnicode(L"\u2654");*/
 }
 
-
-VK, L"\u2654	
-VD, L"\u2655	
-VT, L"\u2656	 
-VL, L"\u2657	
-VR, L"\u2658	
-VS, L"\u2659"
-MK, L"\u265A	
-MD, L"\u265B
-MT, L"\u265C”
-ML, L"\u265D"
-MR, L"\u265E"
-MS, L"\u265F"
+//
+//VK, L"\u2654	
+//VD, L"\u2655	
+//VT, L"\u2656	 
+//VL, L"\u2657	
+//VR, L"\u2658	
+//VS, L"\u2659"
+//MK, L"\u265A	
+//MD, L"\u265B
+//MT, L"\u265C”
+//ML, L"\u265D"
+//MR, L"\u265E"
+//MS, L"\u265F"
