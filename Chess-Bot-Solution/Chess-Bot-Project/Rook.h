81487cc77fda23e49207f8a774a7d1f0b1382069
topias paljakka <topias-paljakka@hotmail.com>
2019-02-08 10:17:56 +0200

    Rook.h better than teachers

--- Chess-Bot-Solution/Chess-Bot-Project/Rook.h
+++ Chess-Bot-Solution/Chess-Bot-Project/Rook.h
@@ -6,16 +6,31 @@
 
 class Rook : virtual public ChessPiece
 {
-	virtual void getMoves(
-		std::list<Move>& list,
-		Tile* tile,
-		Position* position,
-		int color
-	)
+	virtual void getMoves(std::list<Move>& list, Tile* tile, Position* position, int color)
 	{
 		//nykyiset
+		int row = tile->getRow();
+		int column = tile->getColumn();
+
 		// oikealle
-			//uudet
+		for (int i = column + 1; i < 7; i++)
+		{
 			//mitä ruudussa on (tyhjä tai vastustajan nappula
+			ChessPiece* n = position->board[row][i];
+			
+			if (n == nullptr)
+			{
+				list.push_back(Move(Tile(row, column), Tile(row, i)));
+				continue;
+			}
+
+			// onko vastustajan nappula?
+			if (n->getColor() != color)
+			{
+				list.push_back(Move(Tile(row, column), Tile(row, i)));
+			}
+
+			break;
+		}
 	}
 }
